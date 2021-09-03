171334afec08b4b1abd2939518270c9a3b773393
Yivan <yivan.padsoyan.yp@gmail.com>
2021-09-03 19:59:09 +0800

    Minor stuff

--- src/Pikotaro/PPAP.dart
+++ src/Pikotaro/PPAP.dart
@@ -1,7 +1,7 @@
-void PPAP() {
+PPAP() {
   var applePen = get(pen) + get(apple);
   var pineapplePen = get(pen) + get(pineapple);
   var penPineappleApplePen = applePen + pineapplePen;
 
-  penPineappleApplePen;
+  return penPineappleApplePen;
 }
